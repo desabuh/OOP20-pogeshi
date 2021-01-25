209c74a15cabdf1c1414774aa5b2d0b7c821a626
Claudio Verazza <claudio.verazza@studio.unibo.it>
2021-01-25 17:25:03 +0100

    Added the current turn. Basic implementation of turn switching

--- src/main/java/models/BattleImpl.java
+++ src/main/java/models/BattleImpl.java
@@ -1,10 +1,17 @@
 package models;
 
 public class BattleImpl implements Battle {
+    
+    private Turn turn = Turn.PLAYER;
 
     @Override
     public void endTurn() {
-        // TODO Auto-generated method stub
+        if(turn == Turn.PLAYER) {
+            turn = Turn.ENEMY;
+        }
+        else {
+            turn = Turn.PLAYER;
+        }
     }
 
     @Override
