fd43a3cd61371a013feef4f67b14f873cd1d1483
Claudio Verazza <claudio.verazza@studio.unibo.it>
2021-01-30 17:34:50 +0100

    Cards are momentarily simulated through a button. Added a basic event handler to the button

--- src/main/java/controllers/BattleControllerImpl.java
+++ src/main/java/controllers/BattleControllerImpl.java
@@ -1,16 +1,42 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import models.*;
 
 public class BattleControllerImpl implements BattleController {
     
+    private Player p = new PlayerImpl(30);
+    private Battle b = new BattleImpl();
+
     @FXML
     private Label LBLPlayerHealth;
+    @FXML
+    private HBox HBPlayerHand;
 
     @FXML
     public void initialize() {
-       LBLPlayerHealth.setText("10");
+        p.addCard(new CardImpl("Carta prova", 2, 3, 0));
+        p.addCard(new CardImpl("Carta prova 2", 1, 1, 0));
+        LBLPlayerHealth.setText(String.valueOf(p.getHealth()));
+        for(Card c : p.getHand()) {
+            Button b = new Button(c.getName());
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    selectedCard(c);
+                }
+                
+            });
+            HBPlayerHand.getChildren().add(new Button(c.getName()));
+        }
+    }
+    
+    public void selectedCard(Card c) {
+        
     }
     
 }
