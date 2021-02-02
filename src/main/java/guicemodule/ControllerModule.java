ace594676ecf46cdb3af3d861837a05934470014
Sabuh <mastersabuh@gmail.com>
2021-02-02 13:01:59 +0100

    SceneManager class

--- src/main/java/guicemodule/ControllerModule.java
+++ src/main/java/guicemodule/ControllerModule.java
@@ -1,4 +1,4 @@
-package guicemodules;
+package guicemodule;
 
 
 
---
+++ src/main/java/views/scene/SceneManager.java
@@ -0,0 +1,102 @@
+package view.scene;
+
+
+
+
+
+
+import java.io.IOException;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.util.Callback;
+
+/**
+ *   class to provide basic loader functionality and scene wrapping.
+ */
+public final class SceneManager {
+
+    /**
+     * static mapper to return a SceneManager to work on the specific layout.
+     */
+    public static final Map<LAYOUTS, SceneManager> MANAGERS = new HashMap<>();
+
+    /**
+     * static factory rule to provide a controller for the current Loader.
+     */
+    public static Callback<Class<? extends Controller>, Controller> FACTORY;
+
+    /**
+     * actual Scene instance.
+     */
+    private final Scene providedScene;
+
+    /**
+     * loader used.
+     */
+    private final FXMLLoader loader;
+
+    /**
+     * static factory to provide a SceneManager instance.
+     * @param layout enum related to layout to load
+     * @return SceneManager instance
+     */
+    public static SceneManager of(final LAYOUTS layout) {
+        return MANAGERS.computeIfAbsent(layout, ignored -> new SceneManager(ignored));
+    }
+
+    /**
+     * private constructor to create a new SceneManager.
+     * @param layout enum related to layout to load
+     */
+    private SceneManager(final LAYOUTS layout) {
+        this.loader = new FXMLLoader();
+        this.loader.setControllerFactory(FACTORY);
+        Parent parent = loader.load(layout);
+        this.providedScene =  new Scene(parent);
+    }
+
+    /**
+     * retrieve scene instance.
+     * @return providedScene
+     */
+    public Scene getScene() {
+        return this.providedScene;
+    }
+
+    /**
+     * retrieve loader instance.
+     * @return loader
+     */
+    public FXMLLoader getLoader() {
+        return this.loader;
+    }
+
+    /**
+     * static method used to provide an alternative factory to the standard one for providing controllers.
+     * @param controllerFactory alternative controller factory to provide controllers
+     */
+    public static void setControllerFactoryProvide(final Callback<Class<? extends Controller>, Controller> controllerFactory) {
+        FACTORY = controllerFactory;
+    }
+
+    /*1)PS provide a builder to create the scene, 2)wrap scene object*/
+    /**
+     * method used to switch to another scene(*working*).
+     * @throws IOException if an error occured when loading the layout
+     * @param layout layout enum provided
+     * @return scene to be loaded
+     */
+    public Scene loadScene(final LAYOUTS layout) throws IOException {
+        final Parent sceneRoot = FXMLLoader.load(layout);
+        return new Scene(sceneRoot, 100, 100);
+    }
+
+
+}
