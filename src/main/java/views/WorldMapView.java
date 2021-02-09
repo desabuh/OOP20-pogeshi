2d40f17f76915e8c7b57a2d5bafa04927f32ffbc
Sabuh <mastersabuh@gmail.com>
2021-02-09 22:56:41 +0100

    added WorldMapView, comp. Drawer<Canvas>

---
+++ src/main/java/views/WorldMapView.java
@@ -0,0 +1,26 @@
+package views;
+
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+import views.drawer.CanvasDrawer;
+import views.drawer.Drawer;
+import views.render.Render;
+
+public final class WorldMapView extends JavafxView {
+
+
+    private Drawer<Canvas> drawer;
+
+    public WorldMapView(final Stage stage) {
+        super(stage);
+        this.drawer = new CanvasDrawer((Canvas) this.getScene().lookup("#canvasMap"));
+    }
+
+     void updateEntity(final Render render) {
+        this.drawer.draw(render);
+    }
+
+
+
+
+}
