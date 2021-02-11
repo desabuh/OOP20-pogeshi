91424293a7526284f8fe4f7f36f5e18876c3e179
Sabuh <mastersabuh@gmail.com>
2021-02-11 22:09:57 +0100

    added WorlMapView implementation

--- src/main/java/views/WorldMapView.java
+++ src/main/java/views/WorldMapView.java
@@ -1,24 +1,32 @@
 package views;
 
+import com.google.inject.Inject;
+
 import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import views.drawer.CanvasDrawer;
 import views.drawer.Drawer;
 import views.render.Render;
+import views.scene.layout.LAYOUT;
 
 public final class WorldMapView extends JavafxView {
 
+    /**
+     * standard layout to provide scene for this view.
+     */
+    //public static final LAYOUT WORLD_LAYOUT = LAYOUT.WORLDMAP;
 
     private Drawer<Canvas> drawer;
 
+    //@Inject
     public WorldMapView(final Stage stage) {
-        super(stage);
-        this.drawer = new CanvasDrawer((Canvas) this.getScene().lookup("#canvasMap"));
+        super(stage, LAYOUT.WORLDMAP);
     }
 
-    void updateEntity(final Render render) {
-       Platform.runLater(() -> this.drawer.draw(render));
+    public void updateEntity(final Render render) {
+        this.drawer = new CanvasDrawer((Canvas) this.getScene().lookup("#canvasMap"));
+        Platform.runLater(() -> this.drawer.draw(render));
     }
 
 
