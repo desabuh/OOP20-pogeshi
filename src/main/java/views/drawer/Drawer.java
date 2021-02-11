73b6d3af8c63543dfb94b0a28e5dd8af06584af1
Sabuh <mastersabuh@gmail.com>
2021-02-11 21:58:48 +0100

    small correction into Drawer interface

--- src/main/java/views/drawer/Drawer.java
+++ src/main/java/views/drawer/Drawer.java
@@ -1,13 +1,14 @@
 package views.drawer;
 
+import javafx.scene.Node;
 import views.render.Render;
 
 /**
  * 
  * interface to perform drawing operation on view.
- * @param <T> component where drawing is performed
+ * @param <T> component where drawing is performed(must be a javafx Scene node)
  */
-public interface Drawer<T> {
+public interface Drawer<T extends Node> {
 
     /**
      * 
