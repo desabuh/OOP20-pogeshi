49328f07c78d8664b28c7669bb4bcc4039983ed4
Sabuh <mastersabuh@gmail.com>
2021-02-11 22:06:07 +0100

    added updateEntity to View contract

--- src/main/java/views/View.java
+++ src/main/java/views/View.java
@@ -1,5 +1,6 @@
 package views;
 
+import views.render.Render;
 import views.scene.layout.LAYOUT;
 
 /**
@@ -11,8 +12,14 @@ public interface View {
 
     /**
      * load a Scene inside this view.
-     * @param layout layout to load as scene
+     * @param layout layout to load for
      */
     void loadScene(LAYOUT layout);
+
+    /**
+     * a method provided to display or update an entity by its render.
+     * @param render of the entity to display
+     */
+    void updateEntity(Render render);
 
 }
