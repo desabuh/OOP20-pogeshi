50d0a3aeab35f2a957c7770fbceb4d87b9d799df
Sabuh <mastersabuh@gmail.com>
2021-02-14 16:11:20 +0100

    testing key for WorldMapController

--- src/main/java/controllers/WorldMapController.java
+++ src/main/java/controllers/WorldMapController.java
@@ -1,13 +1,12 @@
 package controllers;
 
-
-
-import java.util.concurrent.TimeUnit;
-
 import com.google.inject.Inject;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import models.GameMap.WorldMap;
 import views.View;
 import views.render.Render;
@@ -32,10 +31,29 @@ public final class WorldMapController implements Controller {
          */
         Platform.runLater(() ->  this.worldView.setScene(SceneManager.of(LAYOUT.WORLDMAP).getScene()));
     }
-    
+
 
     @FXML
     void drawPlayer() {
-        this.worldView.updateEntity(new Render(100, 100, 1));
+        this.worldView.updateEntity(new Render(100, 100, 1), new Point2D(500,500), new Point2D(500,500));
+    }
+
+    @FXML
+    void movePlayer(final KeyEvent keyEvent) {
+        /*temporany for testing, add enum*/
+        if (keyEvent.getCode().equals(KeyCode.W)) {
+            this.worldView.updateEntity(new Render(100, 100, 1), new Point2D(500,500), new Point2D(500,400));
+        }
+        if (keyEvent.getCode().equals(KeyCode.S)) {
+            this.worldView.updateEntity(new Render(100, 100, 1), new Point2D(500,500), new Point2D(500,600));
+        }
+        if (keyEvent.getCode().equals(KeyCode.E)) {
+            this.worldView.updateEntity(new Render(100, 100, 1), new Point2D(500,500), new Point2D(600,500));
+        }
+        if (keyEvent.getCode().equals(KeyCode.S)) {
+            this.worldView.updateEntity(new Render(100, 100, 1), new Point2D(500,500), new Point2D(400,500));
+        }
+
+
     }
 }
