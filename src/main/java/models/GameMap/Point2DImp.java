36a59e6aaa386e12cb15d922f623c88522c666fc
Francesco <emailperlascuolamia@gmail.com>
2021-02-17 19:34:10 +0100

    Moved: Point2D and Point2DImp to GameMap/ For correct packeging reasons.

--- src/main/java/models/GameMap/Point2DImp.java
+++ src/main/java/models/GameMap/Point2DImp.java
@@ -1,35 +1,54 @@
-package models;
+package models.GameMap;
 
 public final class Point2DImp implements Point2D {
 
     private int x;
     private int y;
 
+    /**
+     * Set {@code this.X} and {@code this.Y} equals to x and y.
+     * @param x
+     * @param y
+     */
     public Point2DImp(final int x, final int y) {
-        this.x = x;
-        this.y = y;
+        setBoth(x, y);
     }
 
+    /**
+     * Return {@code X}.
+     */
     @Override
     public int getX() {
         return x;
     }
 
+    /**
+     * Return {@code Y}.
+     */
     @Override
     public int getY() {
         return y;
     }
 
+    /**
+     * Set {@code this.X} equal to x. 
+     */
     @Override
     public void setX(final int x) {
         this.x = x;
     }
 
+    /**
+     * Set {@code this.Y} equal to y.
+     */
     @Override
     public void setY(final int y) {
         this.y = y;
     }
 
+    /**
+     * Set {@code this.X} and {@code this.y} equals to x and y.
+     */
     @Override
     public void setBoth(final int x, final int y) {
         this.x = x;
@@ -37,11 +56,17 @@ public final class Point2DImp implements Point2D {
 
     }
 
+    /**
+     * Add the x and y values of {@code value} to {@code this.X} and {@code this.Y}.
+     */
     @Override
     public void sum(final Point2D value) {
         sum(value.getX(), value.getY());
     }
 
+    /**
+     * Add the x and y values to {@code this.X} and {@code this.Y}.
+     */
     @Override
     public void sum(final int x, final int y) {
         this.setX(this.getX() + x);
