ba24088426313a60c3773a6070fdab9dad7ef8d3
Claudio Verazza <claudio.verazza@studio.unibo.it>
2021-02-18 18:18:35 +0100

    The player's deck is now traveled using an iterator rather than taking a card through the deck itself. Added a common function to inflict damage based on the current turn. Fixed a bug where the program would crash if the enemy emptied its deck. When a battle finished, no more operations can be performed.

--- src/main/java/models/BattleImpl.java
+++ src/main/java/models/BattleImpl.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.Iterator;
 import java.util.Optional;
 
 public final class BattleImpl implements Battle {
@@ -15,9 +16,10 @@ public final class BattleImpl implements Battle {
     private int playerUnusedCombatMana;
     private Turn turn = Turn.PLAYER;
     private boolean hasBeenInitialized = false;
+    private boolean hasBattleFinished = false;
     private Player p;
     private EnemyImp e;
-    private Account account = new AccountImp();
+    private Iterator<Card> deckIterator;
 
     public BattleImpl(final Player p, final EnemyImp e) {
         this.p = p;
@@ -29,8 +31,15 @@ public final class BattleImpl implements Battle {
         e = new EnemyImp(new DeckImpl(), new Point2DImp(0, 0));
     }
 
+    public BattleImpl(final Player p) {
+        this.p = p;
+        e = new EnemyImp(new DeckImpl(), new Point2DImp(0, 0));
+    }
+
+
     @Override
     public void endTurn() {
+        checkBattleStatus();
         if (!hasBeenInitialized) {
             throw new IllegalStateException("The opponents have not been initialized.");
         }
@@ -43,11 +52,11 @@ public final class BattleImpl implements Battle {
     }
 
     public void initializeCharacters() {
+        checkBattleStatus();
+        deckIterator = p.getDeck().getCards().iterator();
         if (hasBeenInitialized) {
             throw new IllegalStateException("Both opponents of the battle have already been initialized.");
         }
-        loadPlayersDeck();
-        //e.getDeck().addCard(new CardImpl.Builder().attack(10).shield(0).cost(1).description("").resourcePath("res" + File.separator + "images" + File.separator + "AceOfHearts.png").name("Sample enemy card").build());
         loadEnemysDeck();
         for (int i = 0; i < BASE_STARTING_CARDS; i++) {
             drawCard();
@@ -57,38 +66,38 @@ public final class BattleImpl implements Battle {
     }
 
     public boolean playCard(final int index) {
+        checkBattleStatus();
         if (!hasBeenInitialized) {
             throw new IllegalStateException("The opponents have not been initialized.");
         }
         Card played;
-        if (turn == Turn.PLAYER && isPlayable(p.getHand().getCards().get(index))) {
-            if (turn == Turn.PLAYER) {
+        if (turn == Turn.PLAYER) {
+            if (isPlayable(p.getHand().getCards().get(index))) {
                 played = p.getHand().getCards().get(index);
                 System.out.println("Player!");
                 setPlayerUnusedCombatMana((getPlayerUnusedCombatMana() - played.getCost()));
                 if (played.getShield() > 0) {
                     p.setShield(p.getShield() + played.getShield());
                 }
-                inflictDamage(e, played);
+                inflictDamage(played);
                 p.getHand().removeCard(index);
             } else {
-                Optional<Card> next = e.getDeck().popCard();
-                if (next.isPresent()) {
-                    played = next.get();
-                    if (played.getShield() > 0) {
-                        e.setShield(e.getShield() + played.getShield());
-                    }
-                    inflictDamage(p, played);
-                } else {
-                    // TODO: regenerate deck
-                    loadEnemysDeck();
-                }
-                System.out.println("Enemy!");
+               return false;
             }
-            return true;
         } else {
-            return false;
+            System.out.println("Enemy!");
+            Optional<Card> next = e.getDeck().popCard();
+            if (next.isPresent()) {
+                played = next.get();
+            } else {
+                throw new IllegalStateException("Tried to draw a card from empty deck");
+            }
+            if (e.getDeck().getCards().isEmpty()) {
+                loadEnemysDeck();
+            }
+            inflictDamage(played);
         }
+        return true;
     }
 
     @Override
@@ -103,14 +112,14 @@ public final class BattleImpl implements Battle {
     @Override
     public boolean checkBattleEnd() {
         if (p.getHealth() <= 0) {
-            account.lose();
-            return true;
+            //TODO: Transfer loss status to the AccountController
+            //account.lose();
+            hasBattleFinished = true;
         } else if (e.getHealth() <= 0) {
-            account.win();
-            return true;
+            //TODO: Transfer win status to the MainController
+            hasBattleFinished = true;
         }
-        return false;
-        //return p.getHealth() <= 0 || e.getHealth() <= 0;
+        return hasBattleFinished;
     }
 
     public Player getPlayer() {
@@ -129,42 +138,61 @@ public final class BattleImpl implements Battle {
         playerUnusedCombatMana = amount;
     }
 
-    private void inflictDamage(final Character target, final Card c) {
+    private void inflictDamage(final Card c) {
         int damage = c.getAttack();
-        if (target.getShield() > damage) {
-            target.setShield(target.getShield() - damage);
-            damage = 0;
-        } else if (target.getShield() > 0) {
-            damage -= target.getShield();
-            target.setShield(0);
+        if (turn == Turn.PLAYER) {
+            if (e.getShield() > damage) {
+                p.setShield(e.getShield() - damage);
+                damage = 0;
+            } else if (e.getShield() > 0) {
+                damage -= e.getShield();
+                e.setShield(0);
+            }
+            e.setHealth(e.getHealth() - damage);
+            if (c.getShield() > 0) {
+                p.setShield(p.getShield() + c.getShield());
+            }
+        } else {
+            if (p.getShield() > damage) {
+                p.setShield(p.getShield() - damage);
+                damage = 0;
+            } else if (p.getShield() > 0) {
+                damage -= p.getShield();
+                p.setShield(0);
+            }
+            p.setHealth(p.getHealth() - damage);
+            if (c.getShield() > 0) {
+                e.setShield(e.getShield() + c.getShield());
+            }
+
         }
-        target.setHealth(target.getHealth() - damage);
     }
 
     private void drawCard() {
-        if (turn == Turn.PLAYER && p.getHand().getCards().size() <= MAX_CARDS_IN_HAND) {
-            Optional<Card> next = p.getDeck().popCard();
-            if (next.isPresent()) {
-                p.getHand().addCard(next.get());
-            } else {
-                loadPlayersDeck();
-                /**
-                 * Since the maximum amount of cards in the player's hand is less than the amount of total cards in the deck,
-                 * after a deck refill there will always be at least 1 card in the deck.
-                 * */
-                p.getHand().addCard(p.getDeck().popCard().get());
+        Card next;
+        while (true) {
+            if (!deckIterator.hasNext()) {
+                deckIterator = p.getDeck().getCards().iterator(); 
+            }
+            next = deckIterator.next();
+            if (!p.getHand().getCards().contains(next)) {
+                break;
             }
         }
+        if (turn == Turn.PLAYER && p.getHand().getCards().size() < MAX_CARDS_IN_HAND) {
+            p.getHand().addCard(next);
+        }
     }
+
 
     /**
      * Gets the player's current saved deck and add to the active deck every card that is not already in the player's hand.
      * */
-    private void loadPlayersDeck() {
+    /*private void loadPlayersDeck() {
         account.getDeck().getCards().stream()
                                        .filter((card) -> !p.getHand().getCards().contains(card))
                                        .forEach((card) -> p.getDeck().addCard(card));
-    }
+    }*/
 
     /**
      * Fills the enemy's deck with the default deck's cards.
@@ -174,5 +202,10 @@ public final class BattleImpl implements Battle {
                                                 .forEach((card) -> e.getDeck().addCard(card));
     }
 
+    private void checkBattleStatus() {
+        if (hasBattleFinished) {
+            throw new IllegalStateException("You cannot perform any more actions because the battle already ended.");
+        }
+    }
 
 }
