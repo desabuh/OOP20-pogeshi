eb4dbd5b3e5bf5e18bc40e7744c239c0c16210ec
Claudio Verazza <claudio.verazza@studio.unibo.it>
2021-02-19 17:24:41 +0100

    New class that iterates through a collection of cards and behaves like an iterator

---
+++ src/main/java/models/CardIterator.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public final class CardIterator implements Iterator<Card> {
+
+    private final Collection<Card> deck;
+    private Iterator<Card> iterator;
+
+
+    public CardIterator(final Collection<Card> deck) {
+        this.deck = deck;
+        this.iterator = this.getIterator();
+    }
+
+    private Iterator<Card> getIterator() {
+        return this.deck.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.iterator.hasNext();
+    }
+
+    @Override
+    public Card next() {
+        this.iterator =  this.hasNext() ? this.iterator : this.getIterator();
+        return this.iterator.next();
+    }
+
+}
