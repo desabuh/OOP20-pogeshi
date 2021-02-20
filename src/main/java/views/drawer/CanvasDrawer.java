c1bc20942626c62738974d31753b1eb11baded11
Sabuh <mastersabuh@gmail.com>
2021-02-20 23:17:09 +0100

    added clear functionality in CanvasDrawer

--- src/main/java/views/drawer/CanvasDrawer.java
+++ src/main/java/views/drawer/CanvasDrawer.java
@@ -4,10 +4,11 @@ package views.drawer;
 
 import java.util.Map.Entry;
 
+
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
-import javafx.geometry.Point2D;
+import models.GameMap.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import views.render.Render;
@@ -18,23 +19,28 @@ public final class CanvasDrawer implements Drawer<Canvas> {
     private Canvas canvas;
     private final BiMap<Point2D, Shape> shapeDisplayed = HashBiMap.create();
     private GraphicsContext gc;
-    
-    public static Point2D test;
+
 
     public CanvasDrawer(final Canvas canvas) {
         this.canvas = canvas;
         this.gc = canvas.getGraphicsContext2D();
     }
 
-
+    private void clearShape(final Render render, final Point2D point) {
+        if (render.getHeigth() == 0) {
+            Shape shape = this.shapeDisplayed.get(point);
+            shape.hideShape();
+            this.shapeDisplayed.remove(point);
+        }
+    }
 
     @Override
     public void draw(final Render render, final Point2D source, final Point2D destination) {
-
-        System.out.println(this.shapeDisplayed);
-
-        if (this.shapeDisplayed.containsKey(source)) {
-
+        
+        if (render.getWidth() == 0) {
+            this.clearShape(render, destination);
+        }
+        else if (this.shapeDisplayed.containsKey(source)) {
 
             MovableShape shape = this.shapeDisplayed.entrySet().stream()
                     .filter(x -> x.getKey().equals(source))
@@ -42,26 +48,19 @@ public final class CanvasDrawer implements Drawer<Canvas> {
                     .map(MovableShapeImpl::new)
                     .findAny()
                     .get();
-            
-
 
             this.shapeDisplayed.remove(source);
 
             shape.moveTo((int) destination.getX(), (int) destination.getY());
 
             this.shapeDisplayed.put(destination, shape);
-            
-            this.shapeDisplayed.remove(source);
-            
-            
-            
-            
 
+            this.shapeDisplayed.remove(source);
 
         }
         else {
             this.shapeDisplayed
-            .put(destination, new Rectangle(gc, render.getWidth(), render.getHeigth(), (int) destination.getY(), (int) destination.getX()));
+            .put(destination, new Rectangle(gc, render.getWidth(), render.getHeigth(), (int) destination.getY(), (int) destination.getX(), render.getColor()));
         }
 
     }
