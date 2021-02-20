7f4083dafb72e2a1abfe2cfaddd04ae181f4d4fb
Sabuh <mastersabuh@gmail.com>
2021-02-20 23:22:00 +0100

    WorldMapController basic functionalities added

--- src/main/java/controllers/WorldMapController.java
+++ src/main/java/controllers/WorldMapController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
 
@@ -10,8 +13,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import listener.Listener;
 import models.Character;
+import models.EnemyImp;
 import models.GameMap.Point2D;
 import models.GameMap.Point2DImp;
 import models.GameMap.WorldMap;
@@ -19,28 +24,30 @@ import notifier.EventBus;
 import notifier.GuavaEventBusAdapter;
 import views.View;
 import views.render.Render;
+import views.render.RenderFactory;
 import views.render.TransformPositionFunction;
+import views.render.WorldMapRenderFactory;
 import views.scene.SceneManager;
 import views.scene.layout.LAYOUT;
 
 public final class WorldMapController implements Controller {
-    
+
     /**
      * provide the rapport between a visual movement unit and a logical one.
      */
     public static final int DELTA = 100;
-    
+
     /**
      * function to provide a mapping from logic movement to visual movement.
      */
-    public static final TransformPositionFunction<Point2D, Point2D> DISPLAY_FUN = 
-        p -> p.sum((DELTA - 1) * p.getX(), (DELTA - 1) * p.getY());
-    
-    
+    public static final TransformPositionFunction<Point2D, Point2D> DISPLAY_FUN = p -> p.sum((DELTA - 1) * p.getX(), (DELTA - 1) * p.getY());
+
+
     private View worldView;
     private WorldMap worldMap;
-    private final GuavaEventBusAdapter<Integer> event = new GuavaEventBusAdapter<Integer>(new com.google.common.eventbus.EventBus());
+    private final GuavaEventBusAdapter<Integer> notifier = new GuavaEventBusAdapter<Integer>(new com.google.common.eventbus.EventBus());
     private MainController mc = new MainController();
+    private RenderFactory renderFactory = new WorldMapRenderFactory();
 
     private Point2D pos = Point2DImp.setPoint(0, 0);
 
@@ -48,8 +55,7 @@ public final class WorldMapController implements Controller {
     public WorldMapController(final WorldMap worldMap, final View worldView) {
         this.worldMap = worldMap;
         this.worldView = worldView;
-        this.event.register(mc);
-        System.out.println(this);
+        this.notifier.register(mc);
     }
 
     @FXML 
@@ -57,73 +63,74 @@ public final class WorldMapController implements Controller {
         /**
          * the scene is setted for the view only when javaFX MainThread is loaded
          */
-        Platform.runLater(() ->  this.worldView.setScene(SceneManager.of(LAYOUT.WORLDMAP).getScene()));
+        Platform.runLater(() ->  this.initializeView());
+    }
+
+    private void initializeView() {
+
+        this.worldView.setScene(SceneManager.of(LAYOUT.WORLDMAP).getScene());
+
+        this.worldView.updateEntity(this.renderFactory.renderPlayer(), pos, pos);
+
+        this.worldMap.getEnemies().stream()
+            .map(EnemyImp::getPosition)
+            .map(DISPLAY_FUN::applyTransform)
+            .collect(Collectors.toList())
+            .forEach(p -> this.worldView.updateEntity(this.renderFactory.renderEnemy(), p, p));
     }
 
 
+
+    private void defeatEnemy(final EnemyImp enemy) {
+        this.worldMap.removeEnemy(enemy);
+        Point2D newPos = DISPLAY_FUN.applyTransform(enemy.getPosition());
+        this.worldView.updateEntity(new Render(0, 0, 1, Color.RED), newPos, newPos);
+
+
+        Optional<EnemyImp> boss = this.worldMap.getBoss();
+
+        if (boss.isPresent()) {
+            boss.map(EnemyImp::getPosition)
+                .map(DISPLAY_FUN::applyTransform)
+                .ifPresentOrElse(p -> this.worldView.updateEntity(this.renderFactory.renderEnemyBoss(), p, p), () -> { });
+            
+            this.notifier.notifyListener(1);
+        }
+    }
+
     @FXML
-    void drawPlayer() {
-        //this.worldView.updateEntity(new Render(100, 100, 1), pos, pos);
-        
-       // System.out.println(this.worldMap.getPlayer().getPosition().toString());
-       // System.out.println(this.worldMap.updatePlayerPosition(MOVEMENT.DOWN));
-       // System.out.println(this.worldMap.getPlayer().getPosition().toString());
-
-
-
-        //event.notifyListener(4);
-
-        //event.notifyListener(4);
-        /*
-        event.notifyListener(new Request<Object>() {
-
-            @Override
-            public String getMessageCode() {
-                return "";
-            }
-
-            @Override
-            public Object supplyData() {
-                return null;
-            }
-        }); */
-
+    void interact() {
+        this.worldMap.playerInteract()
+        .ifPresentOrElse(this::defeatEnemy, () -> { });
     }
 
     @FXML
     void movePlayer(final KeyEvent keyEvent) {
-        /*temporany for testing, add enum*/
 
         Optional<Point2D> newPos = Optional.empty();
-        
-        if (keyEvent.getCode().equals(KeyCode.D)) {
-            newPos = this.worldMap.updatePlayerPosition(MOVEMENT.RIGTH);
-            if (newPos.isPresent()) {
-                this.pos = DISPLAY_FUN.applyTransform(newPos.get());
-                //this.worldView.updateEntity(new Render(100, 100, 1), pos, new Point2D(this.pos.getX(), this.pos.getY() - 100));
-            }
-        }
-        
-        /*
-        if (keyEvent.getCode().equals(KeyCode.W)) {
-            this.worldView.updateEntity(new Render(100, 100, 1), pos, new Point2D(this.pos.getX(), this.pos.getY() - 100));
-            this.pos = new Point2D(this.pos.getX(), this.pos.getY() - 100);
-        }
 
-        else if (keyEvent.getCode().equals(KeyCode.S)) {
-            this.worldView.updateEntity(new Render(100, 100, 1), pos, new Point2D(this.pos.getX(), this.pos.getY() + 100));
-            this.pos = new Point2D(this.pos.getX(), this.pos.getY() + 100);
+        if (keyEvent.getCode().equals(KeyCode.D)) { 
+            newPos = this.worldMap.updatePlayerPosition(MOVEMENT.RIGTH); 
         }
 
         else if (keyEvent.getCode().equals(KeyCode.A)) {
-            this.worldView.updateEntity(new Render(100, 100, 1), pos, new Point2D(this.pos.getX() - 100, this.pos.getY()));
-            this.pos = new Point2D(this.pos.getX() - 100, this.pos.getY());
+            newPos = this.worldMap.updatePlayerPosition(MOVEMENT.LEFT);
         }
-        else if (keyEvent.getCode().equals(KeyCode.D)) {
-            this.worldView.updateEntity(new Render(100, 100, 1), pos, new Point2D(this.pos.getX() + 100, this.pos.getY()));
-            this.pos = new Point2D(this.pos.getX() + 100, this.pos.getY());
-        } 
-        */
+
+        else if (keyEvent.getCode().equals(KeyCode.W)) {
+            newPos = this.worldMap.updatePlayerPosition(MOVEMENT.UP);
+        }
+
+        else if (keyEvent.getCode().equals(KeyCode.S)) {
+            newPos = this.worldMap.updatePlayerPosition(MOVEMENT.DOWN);
+        }
+
+
+        if (newPos.isPresent()) {
+            newPos = Optional.of(DISPLAY_FUN.applyTransform(newPos.get()));
+            this.worldView.updateEntity(renderFactory.renderPlayer(), this.pos, newPos.get());
+            this.pos = newPos.get();
+        }
 
 
     }
