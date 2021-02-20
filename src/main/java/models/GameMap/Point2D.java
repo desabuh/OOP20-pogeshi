0f9edd840a2e1c7a7ddd28439195208e4ac5690c
Francesco <emailperlascuolamia@gmail.com>
2021-02-20 17:44:25 +0100

    Resolved: Point2D and Point2DImp. Resolved merge conflicts.

--- src/main/java/models/GameMap/Point2D.java
+++ src/main/java/models/GameMap/Point2D.java
@@ -15,20 +15,26 @@ public interface Point2D {
     int getY();
 
     /**
-     * Set value of coordinate {@code X}.
+     * Set value of coordinate {@code X} and return a new {@code Point2D} with new {@code X} coordinate.
      * @param x     Value to be set for {@code X}.
+     * @return      New {@code Point2D}.
      */
     Point2D setX(int x);
 
     /**
-     * Set value of coordinate {@code Y}.
+     * Set value of coordinate {@code Y} and return a new {@code Point2D} with new {@code Y} coordinate.
      * @param y     Value to be set for {@code Y}.
+     * @return      New {@code Point2D}.
      */
     Point2D setY(int y);
 
     /**
-     * Sum the x and y values to the {@code X} and {@code Y} coordinates of this {@code Point2D}.
+     * Sum the x and y values to the {@code X} and {@code Y} coordinates of this {@code Point2D}
+     * and return a new {@code Point2D} with new {@code X} and {@code Y} coordinates.
      * @param x     x value to add.
      * @param y     y value to add.
+     * @return      New {@code Point2D}.
      */
     Point2D sum(int x, int y);
+
+}
--- src/main/java/models/GameMap/Point2DImp.java
+++ src/main/java/models/GameMap/Point2DImp.java
@@ -1,9 +1,41 @@
 package models.GameMap;
 
+/**
+ * 
+ * immutable class to provide basic operation with 2D coordinate points, implements {@link Point2D}.
+ *
+ */
 public final class Point2DImp implements Point2D {
 
+    /**
+     * x axis coordinate.
+     */
     private int x;
+
+    /**
+     * y axis coordinate.
+     */
     private int y;
+
+    /**
+     * static factory to get a new {@code Point2D} based on its coordinate.
+     * @param x
+     * @param y
+     * @return {@code Point2D} new {@code Point2D} instance 
+     */
+    public static Point2D setPoint(final int x, final int y) {
+        return new Point2DImp(x, y);
+    }
+
+    /**
+     * static function to provide a new sum-wise Point2D. 
+     * @param point1
+     * @param point2
+     * @return Point2D new Point2D instance
+     */
+    public static Point2D sumPoint2d(final Point2D point1, final Point2D point2) {
+        return setPoint(point1.getX() + point2.getX(), point1.getY() + point2.getY());
+    }
 
     /**
      * Set {@code this.X} and {@code this.Y} equals to x and y.
@@ -11,7 +43,8 @@ public final class Point2DImp implements Point2D {
      * @param y
      */
     public Point2DImp(final int x, final int y) {
-        setBoth(x, y);
+        this.x = x;
+        this.y = y;
     }
 
     /**
@@ -31,28 +64,64 @@ public final class Point2DImp implements Point2D {
     }
 
     /**
-     * Set {@code this.X} equal to x. 
+     * Set {@code this.X} equal to x.
      */
     @Override
-    public void setX(final int x) {
-        this.x = x;
+    public Point2D setX(final int x) {
+        return new Point2DImp(x, this.y);
     }
 
     /**
      * Set {@code this.Y} equal to y.
      */
     @Override
-    public void setY(final int y) {
-        this.y = y;
+    public Point2D setY(final int y) {
+        return new Point2DImp(this.x, y);
     }
 
     /**
-     * Add the x and y values to {@code this.X} and {@code this.Y}.
+     * Combine in a sum two Point2D for the applied changes in respective coordinates.
      */
     @Override
-    public void sum(final int x, final int y) {
-        this.setX(this.getX() + x);
-        this.setY(this.getY() + y);
+    public Point2D sum(final int x, final int y) {
+        return setPoint(this.getX() + x, this.getY() + y);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Point2DImp other = (Point2DImp) obj;
+        if (x != other.x) {
+            return false;
+        }
+        if (y != other.y) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Point2DImp [x=" + x + ", y=" + y + "]";
     }
 
 }
+
