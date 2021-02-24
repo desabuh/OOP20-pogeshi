2980637af3e3122fd9aed7f0698f14d4cb856988
Claudio Verazza <claudio.verazza@studio.unibo.it>
2021-02-24 10:24:32 +0100

    Now returns an anonymous render instead of a variable

--- src/main/java/views/render/BattleRenderFactoryImpl.java
+++ src/main/java/views/render/BattleRenderFactoryImpl.java
@@ -18,38 +18,34 @@ public final class BattleRenderFactoryImpl implements RenderFactory {
      * */
     public static final int ENTITY_LAYER = 1;
 
-    private Render r;
     @Override
     public Render renderPlayer() {
-        r = new RenderBuilder()
-                .setHeigth(RENDER_HEIGHT)
-                .setWidth(RENDER_WIDTH)
-                .setLayer(ENTITY_LAYER)
-                .setColor(Color.DARKGREEN)
-                .build();
-        return r;
+        return new RenderBuilder()
+                    .setHeigth(RENDER_HEIGHT)
+                    .setWidth(RENDER_WIDTH)
+                    .setLayer(ENTITY_LAYER)
+                    .setColor(Color.DARKGREEN)
+                    .build();
     }
 
     @Override
     public Render renderEnemy() {
-        r = new RenderBuilder()
-                .setHeigth(RENDER_HEIGHT)
-                .setWidth(RENDER_WIDTH)
-                .setLayer(ENTITY_LAYER)
-                .setColor(Color.BLACK)
-                .build();
-        return r;
+        return new RenderBuilder()
+                    .setHeigth(RENDER_HEIGHT)
+                    .setWidth(RENDER_WIDTH)
+                    .setLayer(ENTITY_LAYER)
+                    .setColor(Color.BLACK)
+                    .build();
     }
 
     @Override
     public Render renderEnemyBoss() {
-        r = new RenderBuilder()
-                .setHeigth(RENDER_HEIGHT)
-                .setWidth(RENDER_WIDTH)
-                .setLayer(ENTITY_LAYER)
-                .setColor(Color.RED)
-                .build();
-        return r;
+        return new RenderBuilder()
+                    .setHeigth(RENDER_HEIGHT)
+                    .setWidth(RENDER_WIDTH)
+                    .setLayer(ENTITY_LAYER)
+                    .setColor(Color.RED)
+                    .build();
     }
 
 }
