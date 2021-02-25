aab6c25dc4aafb1df750ae007696e1621451a554
Sabuh <mastersabuh@gmail.com>
2021-02-25 17:25:44 +0100

    fixed problem with javadoc

--- src/main/java/listener/CommandListener.java
+++
@@ -1,9 +0,0 @@
-package listener;
-
-/**
- * extends {@link Listener} interface narrowing the listener object only to {@link Command}.
- */
-public interface CommandListener extends Listener<Command> {
-
-    Class<?> getCommandType();
-}
